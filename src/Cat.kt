d5da65a50d781c1812d35490ccae1d0351a422ed
George Hampton <0hampg66@solent.ac.uk>
2025-10-02 14:34:51 +0100

    add weight to the toString()

--- src/Cat.kt
+++ src/Cat.kt
@@ -9,6 +9,6 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     }
 
     override fun toString() : String {
-        return "Name: $name Age: $age"
+        return "Name: $name Age: $age Weight: $weight"
     }
 }
