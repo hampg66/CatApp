0053a11714383ad32a6b63f2478baaaa20766765
George Hampton <0hampg66@solent.ac.uk>
2025-10-02 16:08:16 +0100

    Added amount to eat

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,12 +1,28 @@
-class Cat (val name: String, var age: Int, var weight: Int) {
+class Cat (private val name: String, var age: Int, var weightIn: Int) {
+
+    var weight = 0
+        set(newWeight) {
+            if(newWeight >= 5) {
+                field = newWeight
+            }
+        }
+
+    init {
+        weight = weightIn
+    }
 
     fun walk() {
         weight--
     }
 
-    fun eat() {
-        weight++
-    }
+    fun eat(amount: Int):Boolean {
+        val newWeight = weight + amount
+        if (newWeight<=20) {
+            weight += amount
+            return true}
+        else{
+            return false
+        }}
 
     override fun toString() : String {
         return "Name: $name Age: $age Weight: $weight"
